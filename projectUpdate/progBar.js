d6c5d3455303f8f9606abb13f88dfc9773f16d6e
Luis Garcia <luisgarcia15@my.unt.edu>
2019-04-28 16:15:50 -0500

    pushhh

--- projectUpdate/progBar.js
+++ projectUpdate/progBar.js
@@ -1,4 +1,10 @@
 function sensors(num) {
   document.getElementById("myProgress1").value = num[0];
   document.getElementById("myProgress2").value = num[1];
+  document.getElementById("myProgress3").value = num[2];
+  document.getElementById("myProgress4").value = num[3];
+  document.getElementById("myProgress5").value = num[4];
+  document.getElementById("myProgress6").value = num[5];
+  document.getElementById("myProgress7").value = num[6];
+  document.getElementById("myProgress8").value = num[7];
 }
