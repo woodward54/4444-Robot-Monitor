4476fc343a60afc45940aa7adffdae9c9927eb5f
BasilFHolloway <basilfholloway@gmail.com>
2019-04-30 12:32:33 -0500

    Map changes

--- projectUpdate/js/map.js
+++ projectUpdate/js/map.js
@@ -1,14 +1,14 @@
 var Robocanvas = document.getElementById("Rmap");
-var Roboctx = Robocanvas.getContext("2d");
+var Rctx = Robocanvas.getContext("2d");
 
 var Obscanvas = document.getElementById("Omap");
 var Octx = Obscanvas.getContext("2d");
 
 var Cubecanvas = document.getElementById("Cmap");
-var Cubectx = Cubecanvas.getContext("2d");
+var Cctx = Cubecanvas.getContext("2d");
 
 var Mothercanvas = document.getElementById("Mmap");
-var Motherctx = Mothercanvas.getContext("2d");
+var Mctx = Mothercanvas.getContext("2d");
 
 function fillObstacles(){
 	let i;
@@ -19,29 +19,29 @@ function fillObstacles(){
 	}
 }
 
-function fillCubes(){
-	Cctx.fillColor = "#00FF00";//set color to green
+/*function fillCubes(){
+	Cctx.fillStyle = "#00FF00";//set color to green
 	Cctx.clearRect(0,0,Robocanvas.width,Robocanvas.height);
 	let i;
 	for(i=0;i<6;i++)
 	{
-		if(!obj.cubes[i].pickedUp)
+		if(obj.cubes[i].pickedUp)
 		{
 			Cctx.fillRec(obj.cubes[i].x-3,obj.cubes[i].y-3,6,6);
 		}
 	}
-}
+}*/
 
 function drawMothership(){
-	Mctx.fillColor = "#00FFFF";//set color to teal
-	Mctx.fillRect(obj.motherShip[0]-6,obj.motherShip[1]-6,12,12);//fill rectangle
+	Mctx.fillStyle = "#00FFFF";//set color to teal
+	Mctx.fillRect(obj.motherShip[0]-15,obj.motherShip[1]-6,30,20);//fill rectangle
 }
 
 function Robotstep()
 {
 	Rctx.setTransform(1,0,0,1,0,0);//reset transformation
 	Rctx.clearRect(0,0,Robocanvas.width,Robocanvas.height);
-	Rctx.fillColor = "#FFFFFF";//set color to white
+	Rctx.fillStyle = "#FFFFFF";//set color to white
 	Rctx.translate(x-(Robocanvas.width)/2,y-(Robocanvas.height/2));//move canvas half the distance, robot will be at the center of this
 	Rctx.rotate(Bear);//rotate to new robot bearing
 	Rctx.fillRect((Robocanvas.width/2)-5,(Robocanvas.height/2)-5,10,10);//fill in new robot position
@@ -50,4 +50,4 @@ function Robotstep()
 //set up locations
 fillObstacles();
 //fillCubes();
-//drawMothership();
+drawMothership();
